54cb5be36487e919057b3eac10d32a260e22a2ee
KiraEatsMetal <kiraeatsmetal@gmail.com>
2024-11-10 11:52:29 -0800

    Initial setup

---
+++ src/main.js
@@ -0,0 +1,25 @@
+/*
+Contributors: Kira Way
+Project: Spider QWOP
+*/
+
+let config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 512,
+    render: {
+        pixelArt:true,
+    },
+    physics:{
+        default: 'arcade',
+        arcade: {
+            //debug: true
+        }
+    },
+    scene: [Play]
+}
+
+let game = new Phaser.Game(config);
+
+//define keys
+let key1, key2, key3, key4, key5, key6, key7, key8, key9, key0
---
+++ src/scenes/Template.js
@@ -0,0 +1,30 @@
+class SceneTemplate extends Phaser.Scene {
+    //functions happen in order
+
+    //happens when the scene is instantiated at the start
+    constructor() {
+        super('sceneKey')
+    }
+
+    //happens once every time the scene is restarted/added
+    init() {
+        //can recieve data
+    }
+
+    //load assets
+    preload() {
+
+    }
+
+    //make things
+    create() {
+        //can recieve data
+    }
+
+    //do constantly
+    update() {
+        //automatically fed time and delta
+    }
+
+    
+}
