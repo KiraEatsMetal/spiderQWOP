a5e6494048af2cfa95bd81e63e078d93f4dfb364
KiraEatsMetal <kiraeatsmetal@gmail.com>
2024-12-02 22:47:41 -0800

    added credits scene

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -19,11 +19,26 @@ class Credits extends Phaser.Scene {
     //make things
     create() {
         //can recieve data
+        //text
+        //title
+        this.add.bitmapText(game.config.width*0.5, game.config.height*1/6, 'pixelU', 'CREDITS', 64).setOrigin(.5)
+        this.add.bitmapText(game.config.width/2, game.config.height*2/6, 'pixelU', 'Code, art: Kira Way', 32).setOrigin(.5)
+        this.add.bitmapText(game.config.width/2, game.config.height*2/6 + 32, 'pixelU', '', 32).setOrigin(.5)
+        this.add.bitmapText(game.config.width/2, game.config.height*2/6 + 64, 'pixelU', '', 32).setOrigin(.5)
+        this.add.bitmapText(game.config.width/2, game.config.height*2/6 + 128, 'pixelU', 'Special thanks to my parents,', 32).setOrigin(.5)
+        this.add.bitmapText(game.config.width/2, game.config.height*2/6 + 160, 'pixelU', 'teachers, and Ellie for support', 32).setOrigin(.5)
+        this.add.bitmapText(game.config.width*1/2, game.config.height*5/6 + 32, 'pixelU', 'Press [4] to go to title', 32).setOrigin(.5)
+
+        //define keys
+        key4 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR)
     }
 
     //do constantly
     update() {
         //automatically fed time and delta
+        if(Phaser.Input.Keyboard.JustDown(key4)){
+            this.scene.start('titleScene')
+        }
     }
 
     
